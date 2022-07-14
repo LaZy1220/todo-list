b17353e588cc48c85835cd06b3c65206385e8760
LaZy1220 <4elove4iska@gmail.com>
2022-07-14 17:26:54 +0300

    start app

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,13 @@
+export function TodoList(){
+    const todos = [{title:'Max',complited:false, id:Math.random()},
+    {title:'Max',complited:false, id:Math.random()},
+    {title:'Max',complited:false, id:Math.random()},
+    {title:'Max',complited:false, id:Math.random()},]
+    return(
+        <ul>
+            {
+                
+            }
+        </ul>
+    )
+}
